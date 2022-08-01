18a693e21e916eff7ae93d6d41fa7ed7a3d8daa9
JFLagar <juan_x_96@hotmail.com>
2022-08-01 13:56:24 +0200

    Implemented Button Up and Button held behaviours to command input

--- Unity Project/Skill Issue/Assets/Scripts/Input/CommandInputs.cs
+++ Unity Project/Skill Issue/Assets/Scripts/Input/CommandInputs.cs
@@ -8,10 +8,36 @@ namespace SkillIssue.Inputs
     public class CommandInputs : ICommandInput
     {
         public Character character;
-        public virtual void InputPressed()
+        private bool pressed = false;
+        private float buttonHeld;
+        public void Update()
         {
+            if(pressed)
+            {
+                buttonHeld += (0.1f * Time.deltaTime);
+            }
+            if (buttonHeld >= 0.1)
+            {
+                InputHold(buttonHeld);
+            }
 
         }
+        public void Activate(bool isUp)
+        {
+            if (pressed == false)
+            {
+                InputPressed();
+                pressed = true;
+            }
+            if (isUp == true)
+            {
+                buttonHeld = 0;
+                pressed = false;
+                InputReleased();
+            }
+
+        }
+        public virtual void InputPressed() { }
         public virtual void InputReleased() { }
         public virtual void InputHold(float time) { }
     }
@@ -22,8 +48,14 @@ namespace SkillIssue.Inputs
         {
             character.PerformAttack(AttackType.Light);
         }
-        public override void InputReleased() { }
-        public override void InputHold(float time) { }
+        public override void InputReleased() 
+        {
+            Debug.Log("LightReleased");
+        }
+        public override void InputHold(float time)
+        {
+            Debug.Log("LightHold");
+        }
     }
     public class HeavyInput : CommandInputs
     {
@@ -31,8 +63,14 @@ namespace SkillIssue.Inputs
         { 
             character.PerformAttack(AttackType.Heavy);
         }
-        public override void InputReleased() { }
-        public override void InputHold(float time) { }
+        public override void InputReleased() 
+        {
+            Debug.Log("HeavyReleased");
+        }
+        public override void InputHold(float time) 
+        {
+            Debug.Log("HeavyHold");
+        }
     }
     public class SpecialInput : CommandInputs
     {
@@ -40,15 +78,31 @@ namespace SkillIssue.Inputs
         {
             character.PerformAttack(AttackType.Special);
         }
-        public override void InputReleased() { }
-        public override void InputHold(float time) { }
+        public override void InputReleased() 
+        { 
+        
+        }
+        public override void InputHold(float time) 
+        {
+        
+        }
     }
     public class MovementInput : CommandInputs
     {
         public Vector2 direction;
-        public override void InputPressed() { Debug.Log("pos:" + direction); }
-        public override void InputReleased() { }
-        public override void InputHold(float time) { }
+        public override void InputPressed()
+        {
+            Debug.Log("pos:" + direction); 
+        }
+        public override void InputReleased() 
+        {
+            direction.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            Debug.Log("pos" + direction);
+        }
+        public override void InputHold(float time)
+        {
+            Debug.Log("Holding" + direction);
+        }
     }
 }
 
--- Unity Project/Skill Issue/Assets/Scripts/Input/InputHandler.cs
+++ Unity Project/Skill Issue/Assets/Scripts/Input/InputHandler.cs
@@ -15,9 +15,11 @@ namespace SkillIssue.Inputs
         private SpecialInput specialButton = new SpecialInput();
         public MovementInput movementInput = new MovementInput();
         // Start is called before the first frame update
+        private bool movementUp, lightUp, heavyUp, specialUp;
 
         public CommandInputs movement;
         public CommandInputs input;
+        private Vector2 direction;
         // Update is called once per frame
         private void Start()
         {
@@ -28,34 +30,101 @@ namespace SkillIssue.Inputs
         }
         void Update()
         {
+            lightButton.Update();
+            heavyButton.Update();
+            specialButton.Update();
+            //movement
             movement = HandleMovementInput();
             if (movement != null)
             {
                 movementInput.direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-                movement.InputPressed();
+                movement.Activate(movementUp);
                 commands.Add(movement.ToString());
             }
+
+            //attack
             input = HandleAttackInput();
             if (input != null)
-            { input.InputPressed();
+            {
+                input.Activate(AttackUp());
                 commands.Add(input.ToString());
             }
-
+            direction = movementInput.direction;
         }
 
         CommandInputs HandleAttackInput()
         {
-            if (Input.GetKeyDown(inputs[0])) { return lightButton; }
-            else if (Input.GetKeyDown(inputs[1])){ return heavyButton; }
-            else if (Input.GetKeyDown(inputs[2])) { return specialButton; }
-            else { return null; }
-            
+            //Light
+            if (Input.GetKeyDown(inputs[0]))
+            {
+                lightUp = false;
+                return lightButton; 
+            }
+
+            if (Input.GetKeyUp(inputs[0]))
+            {
+                lightUp = true;
+                return lightButton;
+            }
+            //Heavy
+            else if (Input.GetKeyDown(inputs[1]))
+            {
+                heavyUp = false;
+                return heavyButton;
+            }
+            if (Input.GetKeyUp(inputs[1]))
+            {
+                heavyUp = true;
+                return heavyButton;
+            }
+            //Special
+            else if (Input.GetKeyDown(inputs[2]))
+            {
+                specialUp = false;
+                return specialButton;
+            }
+            if (Input.GetKeyUp(inputs[2]))
+            {
+                specialUp = true;
+                return specialButton;
+            }
+            else
+            { 
+                return null; 
+            }          
         }
         CommandInputs HandleMovementInput()
         {
-            if (Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical")) { return movementInput; }
+            if (Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical"))
+            {
+                movementUp = false;
+                return movementInput;
+            }
+
+            if (Input.GetButtonUp("Horizontal") || Input.GetButtonUp("Vertical"))
+            {
+                movementUp = true;
+                return movementInput;
+            }
             else { return null; }
 
         }
+        bool AttackUp()
+        {
+            if (input == lightButton)
+            {
+                return lightUp;
+            }
+            if (input == heavyButton)
+            {
+                return heavyUp;
+            }
+            if (input == specialButton)
+            {
+                return specialUp;
+            }
+            return false;
+        }
+     
     }
 }
